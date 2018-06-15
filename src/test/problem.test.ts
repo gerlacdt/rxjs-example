15e08dc98627823093771f0a04364641cb034f58
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2018-06-15 10:44:15 +0200

    updated deps

--- src/test/problem.test.ts
+++ src/test/problem.test.ts
@@ -1,26 +1,25 @@
 /* tslint:disable:no-console */
-
-import rx = require('rxjs');
+import * as rx from 'rxjs';
 
 describe('rxjs', () => {
   it('fist example', done => {
     const o1 = rx.Observable.of('1', '2', '3').delay(500);
-    const o2 = rx.Observable
-      .create((observer: rx.Subscriber<string>) => {
-        observer.next('a');
-        observer.complete();
-        return 'a';
-      })
+    const o2 = rx.Observable.create((observer: rx.Subscriber<string>) => {
+      observer.next('a');
+      observer.complete();
+      return 'a';
+    })
       .do(() => console.log('side effect, should only be called once'))
       .delay(300)
       .publishReplay(1)
       .refCount()
       .repeat();
 
-    const o3 = o1.zip(o2, (a, b) => a + b);
+    const o3 = o1.zip(o2, (a: any, b: any) => a + b);
     o3.subscribe(console.log, console.log, () => {
       console.log('all done');
       done();
     });
   });
 });
+
